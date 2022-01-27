66ea952522dbf7e237369140f1e78573e8e3e06e
Marshall Baccari <mbaccari@uw.edu>
2022-01-27 02:12:54 -0800

    wrote a lot of the logic for building employees and changing questions. getting close

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const questions = require('./lib/Questions');
 
-const intern = require('./lib/Intern');
+const Intern = require('./lib/Intern');
 
 const Manager = require('./lib/Manager');
 
@@ -11,16 +11,22 @@ const Engineer = require('./lib/Engineer');
 
 const employees = [];
 
-const createCompany = () => {
+const createCompany = function() {
+
+    createManager();
+
     function createManager() {
         inquirer.prompt(questions.managerQuestions)
         .then(
             // handle questions to create new manager object
-
+            inquirer.prompt(questions.managerQuestions)
+            .then(( ) => {
+                let newManager = new Manager(this.name, this.id, this.email, this.office);
+                employees.push(newManager);
+            })
             
         )
         
-        createTeam();
     }
 
     function createTeam(){
@@ -36,101 +42,40 @@ const createCompany = () => {
                 return questions.engineerQuestions;
             } else if (this.role === "New Intern") {
                 return questions.internQuestions;
-            }else {
-                // end program
             }
         }).then((choice) => {
 
             inquirer.prompt(choice)
-            .then()
+            .then( () => {
+                if(choice.github = null) {
+                    createIntern()
+                } else if(choice.school = null) {
+                    createEngineer;
+                }
+            });
 
         })
 
     };
 
-    function createEngineer(){
+    function createEngineer(name, id, email, github){
 
-        let newEngineer = new Engineer()
-
-        createTeam()
-    };
-
-    function createIntern(){
-
-
-        let newIntern = new 
+        let newEngineer = new Engineer(name, id, email, github);
+        employees.push(newEngineer);
+        console.log(employees);
 
         createTeam()
     };
 
-    function buildTeam(){
-        // build the page
-    };
-}
-// ask for Manager info
-
-    // prompt user for the data
-
-        // then create and store manager object
-        // employees.push(new Manager())
-
-// ask for Engineer info
-
-    // prompt user for the data
-
-        // then create and store engineer data
+    function createIntern(name, id, email, school){
 
 
-// ask for Intern info
+        let newIntern = new Intern(name, id, email, school);
+        employees.push(newIntern);
+        console.log(employees);
+    }
 
-    // prompt user for the data
-
-        // then create and store  intern data
-
-
-// ask for Whats next
-
-    // add employee, add intern, be done
-
-        // if add engineer => ask for engineer info
-
-        // if add intern => ask for intern info
-
-        // if done => be done
-    
-// use info to build html page
-
-
-
-const ManagerQuestions =
-    {message: "Enter this members office number",
-    name: "office",
-    type: "input"};
-
-
-
-
-
-function newEmployee() {
-    inquirer.prompt([
-        { message: "Enter team members name",
-        name: "name",
-        type: "input"},
-
-        {message: "Enter member id number",
-        name: "id",
-        type: "input"},
-        
-        {message: "Select members role",
-        choices:["Manager", "Engineer", "Intern"],
-        name: "role",
-        type: "list"}
-    ]).then(function(employee) {
-
-        const job = `${employee.role}Questions`;
-        
-        
-    })
 }
 
 createCompany();
+
